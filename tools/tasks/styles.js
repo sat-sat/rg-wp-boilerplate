0c3aa2e0003b242a4f89b26c951f6c70b8bfa2ca
satoru-y <satoru@roostergrin.com>
2017-01-29 17:00:09 -0800

    feat(sass): Complete initial sass folder structure and styles task.

--- tools/tasks/styles.js
+++ tools/tasks/styles.js
@@ -1,30 +1,31 @@
 import gulp from 'gulp'
 import plugins from 'gulp-load-plugins'
 import { client, options } from '../config/seed.config'
-import browserSync from 'browser-sync'
-import postscssAssets from 'postcss-assets'
+// import postscssAssets from 'postcss-assets'
 import postcssNext from 'postcss-cssnext'
 import postcssShort from 'postcss-short'
 import postcssUniqueSelectors from 'postcss-unique-selectors'
+import browserSync from 'browser-sync'
 
 const $ = plugins()
 
 gulp.task('sasslint', () => {
-	return gulp.src(client.lint.styles.input)
-		.pipe($.sassLint())
-		.pipe($.sassLint.format())
-		.pipe($.sassLint.failOnError())
+  return gulp.src(client.lint.styles.input)
+    .pipe($.sassLint())
+    .pipe($.sassLint.format())
+    .pipe($.sassLint.failOnError())
 })
 
 gulp.task('styles', ['sasslint'], () => {
-	return gulp.src(client.styles.input)
-		.pipe(!options.minify ? $.sourcemaps.init() : $.util.noop())
-		.pipe($.sass({
-			indentedSyntax: true
-		}))
-		.on('error', $.notify.onError())
-		.pipe($.postcss([
-			// # shorthands
+  return gulp.src(client.styles.input)
+    .pipe($.sassGlob())
+    .pipe(!options.minify ? $.sourcemaps.init() : $.util.noop())
+    .pipe($.sass({
+      indentedSyntax: true
+    }))
+    .on('error', $.notify.onError())
+    .pipe($.postcss([
+      // # shorthands
       postcssShort,
       // # images
       // # https://github.com/assetsjs/postcss-assets
@@ -44,14 +45,17 @@ gulp.task('styles', ['sasslint'], () => {
       }),
       // # replace multiple selectors in one line
       postcssUniqueSelectors
-		]))
-		.pipe(!options.minify ? $.sourcemaps.write() : $.util.noop())
-		.pipe(options.minify ? $.cssnano({
-			autoprefixer: false,
-			normalizeUrl: false,
-			//--------------------
-			mergeRules: false
-		}) : $.util.noop())
-		.pipe($.rename('main.css'))
-		.pipe(gulp.dest('css'))
+    ]))
+    .pipe(!options.minify ? $.sourcemaps.write() : $.util.noop())
+    .pipe(options.minify ? $.cssnano({
+      autoprefixer: false,
+      normalizeUrl: false,
+      // --------------------
+      mergeRules: false
+    }) : $.util.noop())
+    .pipe($.rename('main.css'))
+    .pipe(gulp.dest(client.styles.output))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 })
