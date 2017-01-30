0f244b0e14df9da0b6b7547764cea8914967147d
satoru-y <satoru@roostergrin.com>
2017-01-29 17:18:35 -0800

    feat(gulp): Add watch, delete, and server gulp tasks

--- tools/tasks/watch.js
+++ tools/tasks/watch.js
@@ -1,7 +1,20 @@
 import gulp from 'gulp'
-import { client, options } from '../config/seed.config'
+import { client } from '../config/seed.config'
 
 gulp.task('watch', () => {
-	// styles
-	gulp.watch(client.styles.watch, ['styles'])
+  // icons
+  gulp.watch(client.icons.input.color, ['icons-color'])
+  gulp.watch(client.icons.input.noColor, ['icons-no-color'])
+
+  // styles
+  gulp.watch(client.styles.watch, ['styles'])
+
+  // scripts
+  gulp.watch(client.scripts.watch, ['scripts'])
+
+  // views
+  gulp.watch(client.views.watch, ['views'])
+
+  // images
+  gulp.watch(client.images.watch, ['images'])
 })
